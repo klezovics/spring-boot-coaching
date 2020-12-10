2ce98cd1461ecd33633a63b310045d21ab470f31
aklezovics <arturs.klezovics@raisin.com>
2020-12-10 14:36:53 +0100

    Initial commit

---
+++ src/main/java/com/klezovich/springbootcoaching/SpringBootCoachingApplication.java
@@ -0,0 +1,12 @@
+package com.klezovich.springbootcoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCoachingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/klezovich/springbootcoaching/SpringBootCoachingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.klezovich.springbootcoaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCoachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
